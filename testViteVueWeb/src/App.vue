1415b5c5cc9868b9749039ad6a51fcff35dbe040
hotmaneil <hotmaneil@gmail.com>
2024-09-03 15:53:15 +0800

    修改為關於我

--- testViteVueWeb/src/App.vue
+++ testViteVueWeb/src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">關於我</RouterLink>
       </nav>
     </div>
   </header>
